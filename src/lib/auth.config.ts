0534b55f95306bb15c39e903735595712bb38d97
kasra1993 <kasramahsouli@gmail.com>
2026-05-14 04:34:38 +0400

    fix: add trustHost to auth config to resolve UntrustedHost error on production

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -5,6 +5,7 @@ import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 
 export const authConfig: NextAuthConfig = {
+  trustHost: true,
   session: { strategy: 'jwt' },
   pages: {
     signIn: '/login',
